d5793eae9c2266ef9a7cd13f510c11f2f4754a25
Aisbert <120768818+tryaisbert@users.noreply.github.com>
2025-04-28 15:17:00 -0400

    Add files via upload

---
+++ js/animaciones.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar',{
+    duration: 4000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.content',{
+    duration: 4000,
+    origin: 'right',
+    distance: '500px'
+});
+
+
+
+sr.reveal('.swiper',{
+    duration: 4000,
+    origin: 'left',
+    distance: '600px',
+    delay: 700
+});
+
+
+
